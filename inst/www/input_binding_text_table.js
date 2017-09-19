fcccb0c597ca4767dda1fec51e9c53363e3e56b8
drbulu <bulukani.mlalazi@gmail.com>
2017-09-20 09:25:26 +1000

    Added getParentId() function to the input binding class to encapsulate existing functionality that is already in use in the subscribe() function to construct the primary table event that tells the R code which input element has been activated. This will also be very important since getParentId() will be useful for the initialisation necessary for shinyinputtables to work nicely with the Shiny app modularisation framework (https://shiny.rstudio.com/articles/modules.html).

--- inst/www/input_binding_text_table.js
+++ inst/www/input_binding_text_table.js
@@ -1,6 +1,7 @@
-var editTableInputBinding = new Shiny.InputBinding();
+var textTableInputBinding = new Shiny.InputBinding();
 
 // adapted from:
+// https://github.com/rstudio/shiny/blob/master/srcjs/input_binding_text.js
 // https://shiny.rstudio.com/gallery/custom-input-bindings.html
 // https://www.w3schools.com/tags/att_input_readonly.asp
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Attributes
@@ -13,14 +14,25 @@ var editTableInputBinding = new Shiny.InputBinding();
 
 // An input binding must implement these methods
 // https://github.com/rstudio/shiny/blob/master/srcjs/input_binding_text.js
-$.extend(editTableInputBinding, {
+$.extend(textTableInputBinding, {
 
   // This returns a jQuery object with the DOM element
   // Targets the input cells of class "input-cell" within
   // the parent table container.
   find: function(scope) {
-    //return $(scope).find('.editable_input_table .input-cell');
     return $(scope).find('.input-cell');
+  },
+
+
+  initialize: function(el){
+    // TODO: Implement table element initialisation
+    // once table creation strategy implemented in R code
+  },
+
+  // gets the shiny div container parent of the input table
+  getParentId: function(el){
+    console.log( "getParentId() on element " + el.id + "...");
+    return $(el).parents('div[class^="shiny"]').prop("id");
   },
 
   getId: function(el) {
@@ -35,38 +47,26 @@ $.extend(editTableInputBinding, {
   setValue: function(el, value) {
     el.value = value;
   },
-
-    // Set up the event listeners so that interactions with the
-    // input will result in data being sent to server.
-    // callback is a function that queues data to be sent to
-    // the server.
-          
-    // 1. get shiny parent entity's ID : 
-    // this is the triggered event's main context to alert the table user.
-    // you could cheat and append a hardcoded prefix to avoid depending on div as shiny parent.
-        
-    // 2. Trigger new shiny input change event based on changed input element ('el')
-    // new event ID: ID of the Shiny parent container of 'el' (e.g. div)
-    // new event value: the ID of 'el', which triggered the source event
+  
   subscribe: function(el, callback) {
 
-    $(el).on('keyup.editTableInputBinding input.editTableInputBinding', function(event) {
+    $(el).on('keyup.textTableInputBinding input.textTableInputBinding', function(event) {
       
       console.log( "Event trigger on element: " + el.id);
       
-      var parentID = $(el).parents('div[class^="shiny"]').prop("id");
+      var parentID = textTableInputBinding.getParentId(el);
       Shiny.onInputChange(parentID, el.id);
       callback(true);
     });
     
-    $(el).on('change.editTableInputBinding', function(event) {
+    $(el).on('change.textTableInputBinding', function(event) {
       callback(false);
     });
     
   },
 
   unsubscribe: function(el) {
-    $(el).off('.editTableInputBinding');
+    $(el).off('.textTableInputBinding');
   },
 
   receiveMessage: function(el, data) {
@@ -102,4 +102,4 @@ $.extend(editTableInputBinding, {
   }
 });
 
-Shiny.inputBindings.register(editTableInputBinding);
+Shiny.inputBindings.register(textTableInputBinding);
