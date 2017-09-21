4ad22d3b99e3439f3af6a73ebfb8159169e9d834
drbulu <bulukani.mlalazi@gmail.com>
2017-09-22 08:04:56 +1000

    Resolved bad resource path implementation in textTableInput() UI function. Moved CSS and JS resources to more sensible locations in inst/www.

--- R/table_ui-textInput.R
+++ R/table_ui-textInput.R
@@ -1,22 +1,17 @@
-
-
+#' textTableInput
+#' 
+#'  
+#'    
+#' @export
 textTableInput <- function(tableId){
 
-  # addResourcePath(
-  #   prefix = 'www/shinyinputtables',
-  #   directoryPath = system.file('www', package='shinyinputtables')
-  # )
-  # 
-  # table_css_path <- 'www/shinyinputtables/element_styling_text_table.css'
-  # table_js_path <- 'www/shinyinputtables/input_binding_text_table.js'
-  
   addResourcePath(
-    prefix = 'shinyinputtables',
-    directoryPath = "./inst/www"
+    prefix = 'www',
+    directoryPath = system.file('www', package='shinyinputtables')
   )
   
-  table_css_path <- 'shinyinputtables/element_styling_text_table.css'
-  table_js_path <- 'shinyinputtables/input_binding_text_table.js'
+  table_css_path <- file.path("www", "css", "element_styling_text_table.css")
+  table_js_path <- file.path("www", "js", "input_binding_text_table.js")
   
   shiny::tagList(
     shiny::singleton(
